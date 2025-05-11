b7b1217b63db7ef97e731021026b4b37139f0b2b
Tim Ford <timjosephford@gmail.com>
2025-05-11 11:43:43 +1000

    adding arg functionality

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from stats import (
     chars_dict_to_sorted_list,
 )
 
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
